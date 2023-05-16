44fef000c9aec3df380c1c907b6ed684ed97082b
Logan Halstrom <logan.halstrom@nasa.gov>
2023-05-16 13:46:10 -0700

    more detail in p10k legend

---
+++ zsh/p10k_legend.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Legend for git status symbols in powerlevel10k prompt
+
+echo "POWERLEVEL 10K COMMAND PROMPT LEGEND"
+echo ""
+echo "GIT STATUS"
+echo "⇣42     if behind the remote. (pull to clear)"
+echo "⇡42     if ahead of the remote; if also behind the remote: ⇣42⇡42. (push to clear)"
+echo "⇠42     if behind the push remote."
+echo "⇢42     if ahead of the push remote; no leading space if also behind: ⇠42⇢42."
+echo "*42     if have stashes. ('git stash clear' to clear)"
+echo "'merge' if the repo is in an unusual state."
+echo "~42     if have merge conflicts."
+echo "+42     if have staged changes. (commit to clear)"
+echo "!42     if have unstaged changes. (commit to clear)"
+echo "?42     if have untracked files. (add or ignore files to clear)"
+echo ""─"     if the number of unstaged files is unknown."
